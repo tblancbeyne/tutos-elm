682ac341a104d4b5667ec33e1ead4fd447fbc077
Thibault Blanc Beyne <thibault.blancbeyne@gmail.com>
2019-05-16 20:46:15 +0200

    Counter

---
+++ Counter/src/Main.elm
@@ -0,0 +1,41 @@
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+
+type alias Model = Int
+
+init : Model
+init =
+    10
+
+-- UPDATE
+
+type Msg = Increment | Decrement | Reset
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+        Reset ->
+            0
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "Reset" ]
+    , text (String.fromInt model)
+    ]
